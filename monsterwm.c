c2d558fd1cce64811d769753f11352d79da2288c
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-03-01 19:36:35 +0200

    exit with specified value

--- monsterwm.c
+++ monsterwm.c
@@ -93,7 +93,7 @@ static void moveresize(const Arg *arg);
 static void mousemotion(const Arg *arg);
 static void next_win();
 static void prev_win();
-static void quit();
+static void quit(const Arg *arg);
 static void spawn(const Arg *arg);
 static void swap_master();
 static void switch_mode(const Arg *arg);
@@ -197,7 +197,7 @@ static int xerrorstart(Display *dis, XErrorEvent *ee);
  * currdeskidx  - which desktop is currently active
  */
 static Bool running = True;
-static int nmonitors, off_x, off_y, currmonidx;
+static int nmonitors, off_x, off_y, currmonidx, retval;
 static unsigned int numlockmask, win_focus, win_unfocus, win_infocus;
 static Display *dis;
 static Window root;
@@ -980,7 +980,8 @@ void propertynotify(XEvent *e) {
  * to quit just stop receiving events
  * run is stopped and control is back to main
  */
-void quit(void) {
+void quit(const Arg *arg) {
+    retval = arg->i;
     running = False;
 }
 
@@ -1254,7 +1255,7 @@ int main(int argc, char *argv[]) {
     run();
     cleanup();
     XCloseDisplay(dis);
-    return 0;
+    return retval;
 }
 
 /* vim: set expandtab ts=4 sts=4 sw=4 : */
