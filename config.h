84311089956ce7d54bfd545dcd6bbf662cd71120
Jari Vetoniemi <mailroxas@gmail.com>
2012-09-17 08:12:44 +0300

    Use button1

--- config.h
+++ config.h
@@ -20,7 +20,7 @@
 #define FOLLOW_MONITOR  True      /* follow the window when moved to a different monitor */
 #define FOLLOW_MOUSE    False     /* focus the window the mouse just entered */
 #define CLICK_TO_FOCUS  True      /* focus an unfocused window when clicked  */
-#define FOCUS_BUTTON    Button3   /* mouse button to be used along with CLICK_TO_FOCUS */
+#define FOCUS_BUTTON    Button1   /* mouse button to be used along with CLICK_TO_FOCUS */
 #define BORDER_WIDTH    2         /* window border width */
 #define FOCUS           "#ff950e" /* focused window border color   */
 #define UNFOCUS         "#444444" /* unfocused window border color */
